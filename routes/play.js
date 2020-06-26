20774b1b6d45e6b7191004b76e10bb5bf47cba73
M4ngo3ater <ujjwaldimri@hotmail.com>
2020-06-27 01:14:06 +0530

    added multiple levels

--- routes/play.js
+++ routes/play.js
@@ -5,14 +5,132 @@ const Answer = require("../models/Answer.js");
 const Log = require("../models/Log.js");
 const crypto = require('crypto');
 const jwt = require("jsonwebtoken");
-
+const nodemailer = require('nodemailer');
 const path = require('path');
+
+//set up mailer
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: process.env.GMAIL_EMAIL,
+      pass: process.env.GMAIL_PASSWORD
+    }
+});
+
 /*
 router.get("/play", (req,res) => {
     return res.sendFile(path.join(__dirname + '/../views/playBefore.html'));
 });
 */
 
+//level 6 (constellation)
+function level6(email){
+    let mailOptions = {
+        from: process.env.GMAIL_EMAIL,
+        to: email,
+        subject: "Techathlon",
+        text: 'congratulation',
+        attachments: [{
+            filename: 'hmmmm.png',
+            path: __dirname +"/../public/images/lvl6num.ailfh13.png",
+            cid: 'num'
+       }]
+    };
+    transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+            console.log(error);
+        } else {
+            console.log('Email sent: ' + info.response);
+        }
+    });
+}
+router.get("/play/cygnus",async (req,res) => {
+
+    //check if already logged in
+    if(req.cookies['auth']==null){
+        return res.redirect("/techathlon/login");
+    };
+
+    //get user level
+    let token = req.cookies['auth'];
+    let uid;
+
+    //get id from jwt
+    jwt.verify(token,process.env.JWT_SECRET,(err,authData) => {
+        if(err){
+			return res.status(404).sendFile(path.join(__dirname + '/../views/404.html'));	
+		}
+        uid = authData;
+    });
+    const user = await User.findById(uid);
+    if(!user){
+        res.redirect("/techathlon/logout");
+    }
+    if(user.level != 6){
+        return res.status(404).sendFile(path.join(__dirname + '/../views/404.html'));
+    }
+    return res.sendFile(path.resolve(__dirname + "/../public/images/cygnus.oh3doi.png"));
+});
+
+//level 3 (bill gates)
+router.get("/play/leveltrey",async (req,res) => {
+
+    //check if already logged in
+    if(req.cookies['auth']==null){
+        return res.redirect("/techathlon/login");
+    };
+
+    //get user level
+    let token = req.cookies['auth'];
+    let uid;
+
+    //get id from jwt
+    jwt.verify(token,process.env.JWT_SECRET,(err,authData) => {
+        if(err){
+			return res.status(404).sendFile(path.join(__dirname + '/../views/404.html'));	
+		}
+        uid = authData;
+    });
+    const user = await User.findById(uid);
+    if(!user){
+        res.redirect("/techathlon/logout");
+    }
+    if(user.level != 3){
+        return res.status(404).sendFile(path.join(__dirname + '/../views/404.html'));
+    }
+    return res.send("The Toast Derivation");
+});
+
+//ping pong level
+router.get("/play/pong",async (req,res) => {
+
+    //check if already logged in
+    if(req.cookies['auth']==null){
+        return res.redirect("/techathlon/login");
+    };
+
+    //get user level
+    let token = req.cookies['auth'];
+    let uid;
+
+    //get id from jwt
+    jwt.verify(token,process.env.JWT_SECRET,(err,authData) => {
+        if(err){
+			return res.status(404).sendFile(path.join(__dirname + '/../views/404.html'));	
+		}
+        uid = authData;
+    });
+    const user = await User.findById(uid);
+    if(!user){
+        res.redirect("/techathlon/logout");
+    }
+    if(user.level != 7){
+        return res.status(404).sendFile(path.join(__dirname + '/../views/404.html'));
+    }
+    return res.sendFile(path.join(__dirname + '/../views/pong.html'));
+});
+
+//LEVEL STUFF
 router.get("/play",async (req,res) => {
 
     //check if already logged in
@@ -84,10 +202,10 @@ router.post("/play",async (req,res) => {
         const log = new Log({
             username:user.username,
             level:user.level,
-            input:req.body.answer,
+            input:normalisedAns,
             status:"Incorrect"
         });
-        const savedLog = await log.save();
+        await log.save();
 
         //get level info and send error
         const levelInfo = await Question.findOne({"level":user.level});
@@ -99,14 +217,18 @@ router.post("/play",async (req,res) => {
     const log = new Log({
         username:user.username,
         level:user.level,
-        input:req.body.answer,
+        input:normalisedAns,
         status:"Correct"
     });
-    const savedLog = await log.save();
+    await log.save();
 
     user.level += 1;
     user.lastAnswer = new Date().getTime() + 1000*60*60*5.5;
 
+    //email lvl
+    if(user.level == 6){
+        level6(user.email);
+    } 
     const savedUser = await user.save();
 
     res.redirect("/techathlon/play");
