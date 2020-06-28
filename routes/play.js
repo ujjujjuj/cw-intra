a4d736b542784c7c1ba5afdc934d806d4e969f44
M4ngo3ater <ujjwaldimri@hotmail.com>
2020-06-28 14:30:42 +0530

    rnd

--- routes/play.js
+++ routes/play.js
@@ -179,7 +179,7 @@ router.post("/play",async (req,res) => {
     const user = await User.findOne({_id: uid});
 
     //testing
-    if(user.username != process.env.ADMIN_USER || user.username != "duvylfy_luksq"){
+    if(user.username != process.env.ADMIN_USER && user.username != "duvylfy_luksq"){
         return res.status(404).sendFile(path.join(__dirname + '/../views/playBefore.html'));
     } 
     //normalise and hash user ans
